23db1ce9d134802defac36b0c2ba7bbe260b135e
iktueren <iktuerensemih@gmail.com>
2023-02-24 17:07:46 +0300

    if else elif

--- operators.py
+++ operators.py
@@ -20,3 +20,23 @@ x/=10
 x**=2
 x//=2.5
 x%=3
+
+#karşılaştırma operatörleri
+
+x,y=9,12
+print(x<y) #False döndürür
+print(x>y) # True döndürür
+
+print(x==10) # x 12 olduğu için False döndürür
+print(x!=10) # True olarak döndürür
+
+x=[1,3,5]
+print(3 in x) # yer alıyorsa True almıyorsa False olarak döndürür
+x=[2]
+y=[2]
+
+print(id(x),id(y))  # id değerini yazdırır
+
+print(x is y)# False döndü sebebi id'leri kontrol eder
+
+print(x is not y) #True döndürür
