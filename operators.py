378b8db122d7776bc3887d9651c9469b52024b60
iktueren <iktuerensemih@gmail.com>
2023-02-24 16:33:47 +0300

    operatörler

---
+++ operators.py
@@ -0,0 +1,22 @@
+print(2+3)
+
+print(3.5454+6.8587) # float olarak döndürür
+
+print(5/3)
+print(5//3) #tam sayı olarqak sonucu döndürür
+print(5**3) # üzeri olarak verir
+print(5%3) # sadece kalanı döndürür
+
+#atama operatörleri
+
+x,y=20,30
+print(x,y)
+
+x+=5 # x=x+5
+x+=x+5 # 45 olarak döndürür
+x*=5
+x*=y
+x/=10
+x**=2
+x//=2.5
+x%=3
