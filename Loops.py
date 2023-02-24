a5b2a38cbd659d76855a6f6c1b938eb0db4cd9c4
iktueren <iktuerensemih@gmail.com>
2023-02-25 01:00:29 +0300

    further for samples

---
+++ Loops.py
@@ -0,0 +1,34 @@
+#Loops for ve while
+x=[1,2,3,4,5,6,7,8,9,10,"ktrn"]
+
+for i in x:
+    print(i)
+
+for i in x:
+    print("ktrn") # seri adedince girilen değeri yazdırıyor.
+
+numb=range(0,1010) #son sayı dahil edilmez
+
+for i in numb:
+    print(" olala  this is the {} Loop".format(i))
+ 
+
+
+numb=range[1,1001]
+for i in numb:
+    if i%2==00:
+        print("{} Not Single number")
+    elif i%2==1:
+        print("{} is a Single number" )
+
+
+numb=int(input("please enter a number"))
+write=range(0,numb+1)
+for i in write:
+    print(i)
+
+sum=0
+numb=range(0,100)
+for i in numb:
+    sum=sum+i
+    print(sum)
