599bd7e5a03f01eee5de9c99ba9c04b7666fdcd3
iktueren <iktuerensemih@gmail.com>
2023-02-25 01:35:45 +0300

    escape tools

---
+++ escape tools.py
@@ -0,0 +1,8 @@
+#escape tools \ kullanılabilir
+#\n alt satıra geçer
+#\t tab mesafesi kadar boşluk oluşturur.
+print("He says, \"He is tired\" !!")
+print('people\'s choice')
+print("He says,\n \"He is tired\" !!")
+print("\tHuhu")
+
