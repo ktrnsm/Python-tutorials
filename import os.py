580bbe8489923d20322cc683f11d9247bec0e8e1
iktueren <iktuerensemih@gmail.com>
2023-02-27 01:52:09 +0300

    os dict makedirs rmdir

---
+++ import os.py
@@ -0,0 +1,13 @@
+import os
+
+
+os.makedirs("hello")
+os.rmdir("hello")
+y=('C:\\Users\Şebnem\Desktop\tutorials')
+x=os.listdir(y)
+
+os.system("notpad.exe")
+
+x=os.path.exists("sss.docx")# varsa True yoksa False döndürüyorç
+print(x)
+
