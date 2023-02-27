98fc5e139c6ebeb746c38dcf2a9bb5c436485498
iktueren <iktuerensemih@gmail.com>
2023-02-27 21:21:21 +0300

    decarator

---
+++ decorator.py
@@ -0,0 +1,16 @@
+#decorator
+def Sum(x,y):
+    result=x+y
+    print(result)
+Sum(4,6)
+
+a=Sum # fonskiyonu değişkene atma
+a(4,8)
+
+def func1():
+    print("Hi There")
+
+    def func2()
+        return "all is well"
+    print(func2())
+func1()
