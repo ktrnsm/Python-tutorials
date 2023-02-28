9b6d4ec982146736b1850f593eb516a367b9da45
iktueren <iktuerensemih@gmail.com>
2023-03-01 01:00:12 +0300

    import requests

---
+++ 2nd stage/request.py
@@ -0,0 +1,11 @@
+#76
+#connecting with request to a website
+import json
+import requests
+
+site=requests.get("https://www.google.com/")
+print(site)
+
+
+#print(json.__file__)
+#pypi.org >> diğer kütüphanelere ulaşmak için
