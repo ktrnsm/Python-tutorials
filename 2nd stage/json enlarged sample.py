272106b9e8dd41c4328c24cbecede1447f34a9ee
iktueren <iktuerensemih@gmail.com>
2023-02-28 17:19:05 +0300

    mistake in menu() func corrected

--- 2nd stage/json enlarged sample.py
+++ 2nd stage/json enlarged sample.py
@@ -1,10 +1,9 @@
-#json yapısını veri işleme veri kazıma için kullanacağız.
-#fonkiyona parametre göndererk prje yapılacak
-
 import json
 import re
 import time
 import random
+#json yapısını veri işleme veri kazıma için kullanacağız.
+#fonkiyona parametre göndererk prje yapılacak
 
 class Site:
     def __init__(self):
@@ -18,51 +17,63 @@ class Site:
         if selection=="2":
             self.enroll()
         if selection=="3":
-            self.exit()
+            self.quit()
 
     def menu(self):# 1 ve 3 dışında seçenek seçilmemesi için kontrol kuruyoruz.
-        def control(selection):
+        def kontrol(selection):
             if re.search("[^1-3]",selection):
                 raise Exception("Please select between 1-3")
             elif len(selection)!=1:
                 raise Exception("please insert valid single number")
-            while True:
+        while True:
                 try:
                     selection=input("hi there my name is red\n\n Please select the application\n\n[1]-Enter\n[2]-Enroll\n[3]-Exit\n\n")
-                    control(selection)
+                    kontrol(selection)
                 except Exception as failure:
                     print(failure)
                     time.sleep(2)
                 else:
                     break
-            return selection
+        return selection
             
         
 
     def login(self):
         pass
-    def exit(self):
-        pass
+
     def logincontrol(self):
         pass
+
     def loginsuccess(self):
         pass
+
     def loginfailure(self):
         pass
-    def enroll(self):
+
+    def enroll(self): 
         pass
-    def isenrolled(self):
+
+    def isenrolled(self):  # daha önce kayıt var mı kontrolü
         pass
+
     def activation(self):
         pass
+
     def activationcontrol(self):
         pass
+
     def emailcheck(self):
         pass
+
     def getdatas(self): # json üzerinden veri almak için kullanılacak
         pass
-    def recorddatas(self):
+
+    def recorddatas(self):#kayıt işlemini json veritabanına kaydetme işlemi
         pass
+
+    def quit(self):
+        pass
+
     def menureturn(self):
         pass
 
