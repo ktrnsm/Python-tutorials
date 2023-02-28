305aac2016a91420f28ee00a3a559762afd6d1d8
iktueren <iktuerensemih@gmail.com>
2023-03-01 01:18:11 +0300

    getting info from a website and turning it to json via requests

--- 2nd stage/request.py
+++ 2nd stage/request.py
@@ -3,9 +3,18 @@
 import json
 import requests
 
-site=requests.get("https://www.google.com/")
-print(site)
+site=requests.get("https://jsonplaceholder.typicode.com/todos")
+#answer=site.text str olarak döner
+answer=json.loads(site.text) # list olarak döner    
+for i in answer:
+    print(i["title"])
+print(answer[0]["title"]) # listenin ilk elemanının özelliğine kadar çekebiliyoruz.
+
+#print(type(answer)) 
+#terminal [200] ulaşılabilir anlamındadır.
+
 
 
 #print(json.__file__)
 #pypi.org >> diğer kütüphanelere ulaşmak için
+#pip install requests terimnale yazılır.
