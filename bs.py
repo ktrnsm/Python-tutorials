ec2dd81fda4508fa1cceac60b67dd5348f4d0bc6
iktueren <iktuerensemih@gmail.com>
2023-03-03 02:42:49 +0300

    prettify metodu

--- bs.py
+++ bs.py
@@ -1,6 +1,7 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup # html incelenirken karmaşadan kurtulmak için kullanılır.
 
 code="""<!doctype html>"""
 pars=BeautifulSoup(code,"html.parser")
 write=pars.prettify
+write2=pars.find_all("li")[1] # ilk indeksi getirir. li etiketlerinin tamamını liste biçimde döndürür.
 print(write)
