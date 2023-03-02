bb047d84803fd0c40872866dec5b9a0078809bba
iktueren <iktuerensemih@gmail.com>
2023-03-03 00:53:18 +0300

    class created, functions defined libraries imported

--- 2nd stage/#IMDB.py
+++ 2nd stage/#IMDB.py
@@ -1,1 +1,43 @@
 #IMDB
+import requests
+import json
+import time
+import re
+import textwrap
+
+class Movie:
+    def __init__(self):
+        self.loop=True
+
+    def program(self):
+        pass
+    
+    def Menu(self):
+        pass
+
+    def best250(self):
+        pass
+    
+    def mostpopular(self):
+        pass
+    
+    def soon(self):
+        pass
+    
+    def checkmovie(self):
+        pass
+    
+    def quit(self):
+        pass
+    
+    def menu(self):
+        pass
+
+System=Movie()
+while System.loop:
+    System.program()
+
+
+
+
+        
