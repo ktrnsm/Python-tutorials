4d3a95e64eaa146a503f8a4177f0f51ea6efff76
iktueren <iktuerensemih@gmail.com>
2023-03-07 15:41:59 +0300

    how to import csv and excel file to pandas

---
+++ #99 Pandas via Excel.py
@@ -0,0 +1,7 @@
+#99 Pandas via Excel
+import pandas as pd
+data=pd.read_csv("C:/Users/Şebnem/Desktop/tutorials/USD_TRY.csv")
+print(data)
+
+data2nd=pd.read_excel("C:/Users/Şebnem/Desktop/tutorials/USD_TRY2.xlsx")
+print(data2nd)
