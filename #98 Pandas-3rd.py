cb6c07f77e596f5cc2d56f3f3d52eaa186aa130f
iktueren <iktuerensemih@gmail.com>
2023-03-07 15:03:17 +0300

    how to create a dataframe

--- #98 Pandas-3rd.py
+++ #98 Pandas-3rd.py
@@ -1,2 +1,12 @@
 #98 Pandas-3rd
-#data frame yapısı
+#data frame yapısı bir'den fazla serinin data yapııs içinde toplanması
+
+import pandas as pd
+serial1st=[1,3,5,7,9]
+serial2nd=[0,2,4,6,8]
+#y=pd.Series(serial1st)
+#print(y)
+
+headerserial=dict(product1=serial1st,product2=serial2nd)
+datas=pd.DataFrame(headerserial)
+print(datas)
