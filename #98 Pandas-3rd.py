2326700114784f079788eb1ea5ebe23671ef854f
iktueren <iktuerensemih@gmail.com>
2023-03-07 15:31:00 +0300

    data set structure how to create the data

--- #98 Pandas-3rd.py
+++ #98 Pandas-3rd.py
@@ -10,3 +10,15 @@ serial2nd=[0,2,4,6,8]
 headerserial=dict(product1=serial1st,product2=serial2nd)
 datas=pd.DataFrame(headerserial)
 print(datas)
+df=pd.DataFrame([["apple",10],["banana",15],["cherry",20]],columns=["Products","Price"])
+print(df)
+
+data2nd=([["apple,10"]["banana",15]["cherry",25]])
+header=["Product","Price"]
+line=[1,2,3]
+df2nd=pd.DataFrame(data2nd,columns=header,index=line)
+print(df2nd)
+
+dictnry={"Product":["apple","banana","cherry"],"Price":[10,15,25]}
+data=pd.DataFrame(dictnry)
+print(data)
