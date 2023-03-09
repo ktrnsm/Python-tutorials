e1000a1675e0ae399a6c4a6b88fbd0d9b09d63e2
iktueren <iktuerensemih@gmail.com>
2023-03-10 00:36:27 +0300

    histogram

--- #117 Matplotlab Histogram.py
+++ #117 Matplotlab Histogram.py
@@ -1,1 +1,14 @@
 #117 Matplotlab Histogram
+import matplotlib.pyplot as plt
+import numpy as np
+x=np.random.randn(1000)
+x1=np.random.normal(0,1,1000)
+x2=np.random.normal(-5,0.7,1000)
+plt.hist(x,alpha=0.5)
+plt.hist(x1,alpha=0.5)
+plt.hist(x2,alpha=0.5)
+middle=np.median(x)
+
+plt.hist(x, color="red",edgecolor="black",histtype="bar",rwidth=1)#adımlama stepfilled
+plt.axvline(middle,color="blue")
+plt.show()
