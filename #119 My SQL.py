fe73fcac227902e43cf404ea6a7bb777a5388cb7
iktueren <iktuerensemih@gmail.com>
2023-03-11 00:23:47 +0300

    create, and insert create table

--- #119 My SQL.py
+++ #119 My SQL.py
@@ -4,13 +4,14 @@ import mysql.connector
 data=mysql.connector.connect(
     host="localhost",
     user="root",
-    password="Sehersebnem1.",
-    database="sample"
+    password="Sehersebnem1."
+    #database="sample"
 )
 #print(data)
 
 new=data.cursor()
-#new.execute("CREATE DATABASE sample2")
-new.execute("select*from sample.sales")
-for i in new:
-    print(i)
+new.execute("CREATE DATABASE sample4")
+#new.execute("select*from sample.sales")
+#for i in new:
+#    print(i)
+
