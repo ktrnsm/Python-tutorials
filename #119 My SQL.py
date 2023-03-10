17f4d7c27cab950c47d1dfa61744a49dcb421efa
iktueren <iktuerensemih@gmail.com>
2023-03-11 00:03:08 +0300

    sql cursor, execute, connector.connect

--- #119 My SQL.py
+++ #119 My SQL.py
@@ -1,1 +1,16 @@
 #119 My SQL setup
+import mysql.connector
+
+data=mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Sehersebnem1.",
+    database="sample"
+)
+#print(data)
+
+new=data.cursor()
+#new.execute("CREATE DATABASE sample2")
+new.execute("select*from sample.sales")
+for i in new:
+    print(i)
