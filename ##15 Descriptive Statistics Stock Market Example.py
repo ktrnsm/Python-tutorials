930e95da4fec313473331fba2d5b97941d4dc6bd
iktueren <iktuerensemih@gmail.com>
2023-03-12 21:07:37 +0300

    descripe(), usecols="" , hist(),

--- ##15 Descriptive Statistics Stock Market Example.py
+++ ##15 Descriptive Statistics Stock Market Example.py
@@ -1,5 +1,16 @@
 ##15 
 import pandas as pd
+import matplotlib.pylab as plt
 
-data=pd.DataFrame(pd.read_excel("C:/Users/Şebnem/Desktop/tutorials/DataAnalysis/Stock.xlsx"))
-print(data)
+data=pd.DataFrame(pd.read_excel("C:/Users/Şebnem/Desktop/tutorials/##15Stock.xlsx",usecols=["Erprofit","Clprofit"]))
+#print(data.describe())
+#print(data.mean())
+#print(data.median())
+#print(data.quantile(q=0.25)) # 1. kartiller'i getirir.
+#print (data.skew())
+
+plt.figure()
+plt.title("Stock income value")
+plt.hist(data["Erprofit"],color="g",bins=300,histtype="bar")
+plt.hist(data["Clprofit"],color="g",bins=300,histtype="bar")
+plt.show()
