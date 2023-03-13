fca204cccec38d18622bbfc0782ed5542d4a502f
iktueren <iktuerensemih@gmail.com>
2023-03-14 00:33:55 +0300

    random.choices(Age,k=30 # minimum 30 sample needed

--- ##20 Central limit Theorem.py
+++ ##20 Central limit Theorem.py
@@ -1,1 +1,12 @@
-##20 Central limit Theorem
+##20 Central limit Theorem // 1st
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+#customer age Study
+Age=np.random.uniform(low=18,high=75,size=40000)
+#plt.hist(Age)
+#plt.show()
+#sampling=random.choices(Age,k=10)
+sampling=[np.mean(random.choices(Age,k=30)) for _ in range(1000)]
+plt.hist(sampling)
+plt.show()
