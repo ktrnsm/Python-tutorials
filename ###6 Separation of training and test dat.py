7c5d87070a0227b74a8a5834ff191d5c74c37e6c
iktueren <iktuerensemih@gmail.com>
2023-03-16 01:54:53 +0300

    train_test_split,

--- ###6 Separation of training and test dat.py
+++ ###6 Separation of training and test dat.py
@@ -1,3 +1,17 @@
 ###6 Separation of training and test data
 import pandas as pd
-from 
+import pandas as pd
+from sklearn.model_selection import train_test_split
+data=pd.read_excel("C:/Users/Şebnem/Desktop/tutorials/###6.xlsx")
+#print(data)
+
+#bağımlı ve bağımsız değişkenler ayrılır
+
+y=data["Y"]
+X=data[["X1","X2"]]
+
+#4 veri verir ikisi bağımsız ikisi bağımlı
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2)
+
+print(X_train)
+print(X_test)
