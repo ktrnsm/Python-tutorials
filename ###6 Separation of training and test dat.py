163ea31eecde4a5902d032516c997b684b7f8025
iktueren <iktuerensemih@gmail.com>
2023-03-16 03:01:16 +0300

    small corrections

--- ###6 Separation of training and test dat.py
+++ ###6 Separation of training and test dat.py
@@ -11,7 +11,8 @@ y=data["Y"]
 X=data[["X1","X2"]]
 
 #4 veri verir ikisi bağımsız ikisi bağımlı
-X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2)
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42)
+
 
 print(X_train)
 print(X_test)
