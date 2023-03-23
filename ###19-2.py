901167d92bbc1b06530933401b6fc47556332064
iktueren <iktuerensemih@gmail.com>
2023-03-23 03:23:08 +0300

    real estate pol regression model

---
+++ ###19-2.py
@@ -0,0 +1,20 @@
+###19.2 
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.lineer_model import LinearRegression
+
+original_data=pd.read_csv("C:/Users/Şebnem/Desktop/tutorials/Realestate.csv")
+data=original_data.copy()
+
+data.drop(columns=["No","X6 longitude","X5 latitude","X1 transaction date"],axis=1,inplace=True)
+#print(data.info())
+#sns.pairplot(data)
+#plt.show()
+
+y=data["Y house price of unit area"]
+X=data.drop(columns="Y house price of unit area",axis=1)
+#print(X)
+
+
+
